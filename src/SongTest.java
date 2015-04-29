9b0041dc09d0d1a980175cfa8f6ce18c23496be4
송지영 <j0506i@naver.com>
2015-04-29 16:35:00 +0900

    Songs 올라가랏!

---
+++ src/SongTest.java
@@ -0,0 +1,19 @@
+
+public class SongTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	       Song song2 = new Song( "러빙유", "씨스타", "", null, 0, 0 );
+	       Song song3 = new Song( "여수 밤바다", "버스커 버스커");      
+	       Song song4 = new Song( "Like This", "원더걸스" );  
+
+	       Song s = new Song("좋은날", "아이유", "Real", "이민수", 2010, 3);
+			
+			s.show();
+		
+			song2.show();
+			song3.show();
+			song4.show();
+		}
+
+}
